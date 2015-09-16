89277ed3e8186d81842dc7a73f261c59d25005e4
karronoli <karo+git@karonori.com>
2015-09-16 12:02:40 +0900

    Change styles.
    Add Date by meta
    Add semicolon to js
    Remove not using code
    Change to UTF-8

--- komono/filter_test.js
+++ komono/filter_test.js
@@ -1,20 +1,16 @@
 "use strict";
 
-var $ = function(id){return document.getElementById(id);}
+var $ = function(id){return document.getElementById(id);};
 window.onload = function()
 {
     console.log("start");
-    var img = new Image();
-    img.src = "favicon.jpg";
-    // document.body.appendChild(img);
-    
     try {
-        execute(img);
+        execute($("original"));
     } catch(e) {
         alert(e);
     }
     console.log("end");
-}
+};
 
 var execute = function(org)
 {
@@ -24,7 +20,6 @@ var execute = function(org)
     var context = canvas.getContext("2d");
     context.drawImage(org, 0, 0);
 
-    var to = context.createImageData(org.width, org.height);
     var from = context.getImageData(0, 0, org.width, org.height);
     console.log("start length:%d, width:%d, height:%d, status:%d",
                 from.data.length, org.width, org.height,
@@ -34,9 +29,7 @@ var execute = function(org)
         var G = from.data[i + 1];
         var B = from.data[i + 2];
         var average = getColorAverage(R, G, B);
-        // 直接変更の必要有
-        // to.data[i + 0] = to.data[i + 1] = to.data[i + 2] = 
-        from.data[i + 0] = from.data[i + 1] = from.data[i + 2] = 
+        from.data[i + 0] = from.data[i + 1] = from.data[i + 2] =
            Math.floor(average);
             /*
             var average = getLuminance(R, G, B);
@@ -45,7 +38,7 @@ var execute = function(org)
             else // RGB 平均値が閾値より大きい場合
                 pixelImage.data = [255, 255, 255, 255];  // 白色に設定
             */
-            
+
             /*
             R = R  * 2;
             if (R > 255) R = 255;
@@ -55,31 +48,14 @@ var execute = function(org)
             */
     }
     context.putImageData(from, 0, 0);
+};
 
-    var dummy = $("dummy").getContext("2d");
-    /*
-    // Red Test
-    var dummy_d = dummy.getImageData(0, 0, org.width, org.height);
-    for (var i=0;i < dummy_d.data.length;i+=4) {
-      dummy_d.data[i + 0] = 255;
-      dummy_d.data[i + 1] = 0;
-      dummy_d.data[i + 2] = 0;
-      dummy_d.data[i + 3] = 255;
-    }
-    dummy.putImageData(dummy_d, 10, 10);
-    */
-    // context.clearRect(0, 0, org.width, org.height); // no
-    // context.putImageData(to, 0, 0); // no
-    dummy.putImageData(from, 0, 0);
-    
-}
-
-var getColorAverage = function(r, g, b) 
+var getColorAverage = function(r, g, b)
 {
     return r*0.299 + g*0.587 + b*0.114;
-}
+};
 
 var getLuminance = function(r, g, b)
 {
     return (r + g + b)/3;
-}
+};
